b2116c2bc613b66c738df4d3500759fdd9b7122f
prakaram joshi <prakaramjoshi@gmail.com>
2020-07-20 07:34:28 -0700

    improved motion detector

--- motion_detector.cpp
+++ motion_detector.cpp
@@ -1,7 +1,7 @@
 #include "motion_detector.h"
 #include "MotionEvent.h"
 #include "logger.h"
-// #define DEBUG_LOGS
+//#define DEBUG_LOGS
 motion_detector::motion_detector(BlockingQueue<cv::Mat> *_inq) {
 	m_inq = _inq;
 }
@@ -18,14 +18,20 @@ void motion_detector::run() {
 	int motion_count = 0;
 	int min_motion = MoDetSettings::get_minimum_motion();
 	int min_motion_percent = MoDetSettings::get_min_motion_percent();
+#ifdef DEBUG_LOGS
+	std::size_t frame_id=0;
+	std::size_t last_motion_frame = 0;
+#endif
 	while (m_inq->Remove(&mat)) {
 		if (mat) {
+#ifdef DEBUG_LOGS
+			frame_id++;
+#endif
 			cv::cvtColor(*mat, gray, cv::COLOR_RGB2GRAY);
 			cv::GaussianBlur(gray,
 				diffused_frame,
 				cv::Size(5, 5),
 				2.2);
-			bool motion = false;
 			if (prev_frame.size().area()!=0) {
 				
 				cv::subtract(prev_frame, diffused_frame, diff_frame);
@@ -38,25 +44,27 @@ void motion_detector::run() {
 				double motion_percent = non_zero_area*100.0 / area;
 				
 #ifdef DEBUG_LOGS
-				if(motion_percent>0.0){
-					AceLogger::Log("motion : " + std::to_string(motion_percent));
-				}
+				AceLogger::Log("frame : "+std::to_string(frame_id)+\
+				" motion : " + std::to_string(motion_percent)+\
+				" motion count : "+std::to_string(motion_count)+\
+				" last motion frame : "+std::to_string(last_motion_frame));				
 #endif
 				if (motion_percent>min_motion_percent) {
-					motion = true;
+					motion_count++;
 #ifdef DEBUG_LOGS
-					AceLogger::Log(" Has motion ");
+					last_motion_frame = frame_id;
 #endif
 				}
-			}
-			if(motion){
-				motion_count++;
-			}
-			else{
-				motion_count = 0;
-			}
-			if(motion_count>=min_motion){
-				MotionEvent::set_motion();
+				else{
+					motion_count--;
+					if(motion_count<0){
+						motion_count = 0;
+					}
+				}
+				if(motion_count>=min_motion){
+					MotionEvent::set_motion();
+					motion_count = 0;
+				}
 			}
 			
 			diffused_frame.copyTo(prev_frame);
